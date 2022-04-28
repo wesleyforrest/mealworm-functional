b189402dc4e9aa5b03cd3e566d6ec8e8a11edb12
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-04-28 17:38:08 +0100

    Code along complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,25 @@ import styles from "./App.module.scss";
 import CardFront from "./components/CardFront";
 import CardBack from "./components/CardBack";
 import NavBar from "./components/NavBar";
-
+import Card from "./components/Card";
 import library from "./data/fa-library";
 import recipes from "./data/recipes";
 
 const App = () => {
   const getCardJsx = (recipe) => (
-    <React.Fragment key={recipe}>
-      <CardFront recipe={recipe} />
-      <CardBack recipe={recipe} />
-    </React.Fragment>
+    <div className={styles.card} key={recipe}>
+      <Card recipe={recipe} />
+    </div>
   );
 
-    return (
-      <>
-        <section className={styles.nav}>
-          <NavBar />
-        </section>
-        <section className={styles.content}>
-          {recipes.map(getCardJsx)}
-        </section>
-      </>
-    );
-}
- 
+  return (
+    <>
+      <section className={styles.nav}>
+        <NavBar />
+      </section>
+      <section className={styles.content}>{recipes.map(getCardJsx)}</section>
+    </>
+  );
+};
+
 export default App;
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import styles from "./Card.module.scss";
+import CardFront from "../CardFront";
+import CardBack from "../CardBack";
+
+const Card = (props) => {
+  const [isFaceDown, setIsFaceDown] = useState(false);
+
+  const { recipe } = props;
+  const flipStyles = isFaceDown ? styles.faceDown : "";
+  return (
+    <section
+      className={`${styles.recipeCard} ${flipStyles}`}
+      onClick={() => setIsFaceDown(!isFaceDown)}
+    >
+      <div className={styles.front}>
+        <CardFront recipe={recipe} />
+      </div>
+      <div className={styles.back}>
+        <CardBack recipe={recipe} />
+      </div>
+    </section>
+  );
+};
+
+export default Card;
